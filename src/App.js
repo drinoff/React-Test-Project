8ce8a2d2857a74a424d1ee0ee61d1c3fba83df1c
Drinoff <drinoff@gmail.com>
2021-04-11 18:48:50 +0200

    new favicon, request to DB,and implement the betSlip Page

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 import Container from '@material-ui/core/Container';
+import UlComponent from './Components/UlComponent';
 
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
 import './App.css';
 
 function App() {
+
+  
   return (
     <div className="App">
       <Container style={{ backgroundColor: '#cfe8fc', height: '100vh' }} maxWidth="xl">
-        {/* <Typography component="div"  /> */}
         <Button className = 'mainButton' variant="contained" color="primary">
           Home
         </Button>
         <Button className = 'mainButton' variant="contained" color="primary">
-          Work
+          Bet Slip
         </Button>
         <Button className = 'mainButton' variant="contained" color="primary">
-          Projects
+          More Bets
         </Button>
         <Button className = 'mainButton' variant="contained" color="primary">
           Contact
         </Button>
         <Button className = 'mainButton' variant="contained" color="primary" href="#contained-buttons">
-          Galery
+          Archive
         </Button>
-        <Container className="innerCo"><span>Mallorca FC - Lugo</span></Container>
+        
+        <UlComponent />
 
       </Container>
     </div>
